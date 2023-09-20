a89fd6b220ed036bd470a811586797064a39095d
SCOPP-admin <144900192+SCOPP-admin@users.noreply.github.com>
2023-09-20 14:13:37 +0530

    Update StudentCode_test.py
    3 test cases added

--- StudentCode_test.py
+++ StudentCode_test.py
@@ -2,7 +2,14 @@ import StudentCode;
 from IOWrapper import IOWrapper
 testIO= IOWrapper()
 expectedIO= IOWrapper()
+expectedIO2= IOWrapper()
+expectedIO3= IOWrapper()
 def test_hello():
-    expectedIO.print("Hello World")
-    assert (StudentCode.runner(testIO)).check(expectedIO)
+    expectedIO.print("Y Axis")
+    expectedIO2.print("X Axis")
+    expectedIO3.print("X Axis")
+    assert (StudentCode.runner(testIO,0,2)).check(expectedIO)
+    assert (StudentCode.runner(testIO,2,0)).check(expectedIO2)
+    assert (StudentCode.runner(testIO,2,2)).check(expectedIO3)
     print("Passed")
+    #"X axis" or print "Y Axis"
