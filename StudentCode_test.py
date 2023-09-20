e4224da144107e2519ed8daadcb099807ae680ea
SCOPP-admin <144900192+SCOPP-admin@users.noreply.github.com>
2023-09-20 14:15:08 +0530

    Update StudentCode_test.py
    corrected expctedIO3

--- StudentCode_test.py
+++ StudentCode_test.py
@@ -7,7 +7,7 @@ expectedIO3= IOWrapper()
 def test_hello():
     expectedIO.print("Y Axis")
     expectedIO2.print("X Axis")
-    expectedIO3.print("X Axis")
+    expectedIO3.print("No Axis")
     assert (StudentCode.runner(testIO,0,2)).check(expectedIO)
     assert (StudentCode.runner(testIO,2,0)).check(expectedIO2)
     assert (StudentCode.runner(testIO,2,2)).check(expectedIO3)
